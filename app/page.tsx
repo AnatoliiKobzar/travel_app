7878f93f175204c79e18e539d27223bba752c3ea
KobzarAnatolii <TohaPirat@gmail.com>
2023-11-03 23:39:26 +0200

    add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Camp from '@/components/Camp';
+import Features from '@/components/Features';
+import Gaide from '@/components/Gaide';
+import GetApp from '@/components/GetApp';
+import Hero from '@/components/Hero';
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Gaide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
---
+++ components/Gaide.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Gaide = () => {
+  return <div>Gaide</div>;
+};
+
+export default Gaide;
